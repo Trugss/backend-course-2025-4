24de93170982130920fc74843909102b68fdb910
Oleksandrbc2025-4 <s4sh4feed@gmail.com>
2025-10-21 14:50:25 +0300

    Chapter 2 for file main.js, package-lock.json and updated package.json added

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const { program } = require('commander');
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const { XMLBuilder } = require ('fast-xml-parser');
+const { URL } = require ('url');
 
 program
   .requiredOption('-i, --input <path>', 'шлях до файлу для читання')
@@ -21,6 +23,53 @@ if (!fs.existsSync(inputFile)) {
 
 function startServer() {
   const server = http.createServer(async (req, res) => {
+	  const parsedUrl = new URL(req.url, `http://${host}:${port}`);
+const query = parsedUrl.searchParams;
+
+const maxMpg = parseFloat(query.get('max_mpg'));
+const showCyl = query.get('cylinders') === 'true';
+
+try {
+    const data = await fs.promises.readFile(inputFile, { encoding: 'utf8' });
+    const records = JSON.parse(data);
+
+    const dataArray = Object.keys(records).map(model => ({
+        model: model,
+        ...records[model]
+    }));
+
+    const filteredRecords = dataArray.filter(record => {
+        if (!isNaN(maxMpg)) {
+            return record.mpg < maxMpg;
+        }
+        return true;
+    }).map(record => {
+        const projectedRecord = {
+            model: record.model,
+            ...(showCyl ? { cyl: record.cyl } : {}),
+            mpg: record.mpg
+        };
+
+        return { car: projectedRecord }; 
+    });
+
+    const rootObject = { cars: filteredRecords };
+
+    const builder = new XMLBuilder({
+        arrayNodeName: "car",
+        ignoreAttributes: true,
+        format: true
+    });
+    const xmlData = builder.build(rootObject);
+
+    res.writeHead(200, { 'Content-Type': 'application/xml; charset=utf-8' });
+    res.end(xmlData);
+
+} catch (error) {
+    console.error("Error processing request:", error.message);
+    res.writeHead(500, { 'Content-Type': 'text/plain' });
+    res.end("Internal Server Error: check file format or server configuration.");
+}
   });
 
   server.listen(port, host, () => {
